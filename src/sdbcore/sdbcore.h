f0a13e37e7dc30b683dfca7bda1837e4d1cf937a
Cleyh <unbd_233@qq.com>
2025-07-20 14:21:06 +0800

    【v2】v2优化

--- src/sdbcore/sdbcore.h
+++ src/sdbcore/sdbcore.h
@@ -1,6 +1,6 @@
 #pragma once
 
-class sdbcore 
+class SDbCore 
 {
 
 };
---
+++ src/sdbcore/sdbio.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef size_t DataIdx;
+typedef size_t DataSize;
+
+class SdbIoIndex
+{
+public:
+    
+private:
+    DataIdx mIdx;
+    DataSize mSize;
+};
+
+class SdbIO
+{
+};
